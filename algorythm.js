d07e23bb29064829df0b5c9553770946ca721c15
Kamil Sobieraj <kamil.sobieraj.dev@gmail.com>
2019-04-16 11:44:35 +0200

    Sum all numbers in range

--- algorythm.js
+++ algorythm.js
@@ -1,1 +1,17 @@
-console.log("works!");
+/***************************************************************************
+ *********************** Sum all numbers in range
+ ****************************************************************************/
+//e.g. sumAll([3,1]) should be 6
+
+export default function sumAll(arr) {
+  let sum = 0;
+  const minRange = Math.min(...arr);
+  const maxRange = Math.max(...arr);
+  for (let i = minRange; i <= maxRange; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+console.log(sumAll([3, 1])); //6
+console.log(sumAll([1, 4])); //10
