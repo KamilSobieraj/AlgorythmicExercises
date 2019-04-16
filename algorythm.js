d250e353c61f52c5b54b2c8f6e05c4e91acd7845
Kamil Sobieraj <kamil.sobieraj.dev@gmail.com>
2019-04-16 12:11:35 +0200

    Show unique numbers from two arrays

--- algorythm.js
+++ algorythm.js
@@ -1,6 +1,6 @@
 /***************************************************************************
  *********************** Sum all numbers in range
- ****************************************************************************/
+ ****************************************************************************
 //e.g. sumAll([3,1]) should be 6
 
 export default function sumAll(arr) {
@@ -15,3 +15,20 @@ export default function sumAll(arr) {
 
 console.log(sumAll([3, 1])); //6
 console.log(sumAll([1, 4])); //10
+
+
+/****************************************************************************
+*********************** Shoq unique numbers in two arrays 
+***************************************************************************/
+
+function diffArrays(arr1, arr2) {
+  sumArr = [...arr1, ...arr2];
+
+  let filtered = sumArr.filter(e => !arr1.includes(e) || !arr2.includes(e));
+  console.log(filtered.sort());
+}
+
+arr1 = [1, 2, 3, 7, 8, 9];
+arr2 = [1, 2, 5, 6, 9];
+
+console.log(diffArrays(arr1, arr2)); // [3, 5, 6, 7, 8]
