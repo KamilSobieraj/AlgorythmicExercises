d2359e0e5ab09a8fcb2b8ac124d12dfd97a82623
Kamil Sobieraj <kamil.sobieraj.dev@gmail.com>
2019-05-10 09:41:44 +0200

    Convert decimal to Roman number

--- algorythm.js
+++ algorythm.js
@@ -32,10 +32,47 @@ arr1 = [1, 2, 3, 7, 8, 9];
 arr2 = [1, 2, 5, 6, 9];
 
 console.log(diffArrays(arr1, arr2)); // [3, 5, 6, 7, 8]
-*/
+
 
 function unite(...arr) {
   unitedArr = [].concat(...arguments);
   return [...new Set(unitedArr)].sort();
 }
 console.log(unite([1, 2, 3], [5, 4, 3], [6, 5, 3], [1, 1, 2]));
+
+*/
+
+/****************************************************************************
+ *********************** Convert number to Roman number
+ ***************************************************************************/
+
+function convertToRoman(inputNumber) {
+  decimalValuesArray = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  romanValuesArray = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I"
+  ];
+
+  let convertedNumber = "";
+
+  for (let i = 0; i < decimalValuesArray.length; i++) {
+    while (decimalValuesArray[i] <= inputNumber) {
+      convertedNumber += romanValuesArray[i];
+      inputNumber -= decimalValuesArray[i];
+    }
+  }
+  return convertedNumber;
+}
+console.log(convertToRoman(1036));
+console.log(convertToRoman(97));
